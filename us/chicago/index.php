e142f93a7b4396fbfae6290664d1257fc5774245
Jon Spencer <jon@jonspencer.us>
2014-03-24 17:20:04 -0400

    City Page Tweaks

--- us/chicago/index.php
+++ us/chicago/index.php
@@ -6,6 +6,14 @@ require_once('../../header.php'); ?>
 		<div class="darken" class="yep">
 			<div id="subHeader" class="teehee">
 				<div class="wrap">
+					
+					<ul id="breadcrumbs">
+						<li class="marker"></li>
+						<li>
+							<a title="You're Here!" href="/us/chicago/">Chicago</a> 
+						</li>
+					</ul><!-- END BREADCRUBS -->
+					
 					<ul class="menu">
 						<li>
 							<a href="index.php">Accomodations</a>
@@ -42,16 +50,79 @@ require_once('../../header.php'); ?>
 			<div class="wrap">	
 				<div class="featuredPost yep">
 					<a class="featuredTitle yep">Hop On Hop Off Trolley Tour</a>
-					<div class="featuredExcerpt yep">Voted Chicago’s best tour, this Hop on Hop Off Trolley Tour has long been the gold standard for touring downtown Chicago.
+					<div class="featuredExcerpt yep">Voted Chicago's best tour, this Hop on Hop Off Trolley Tour has long been the gold standard for touring downtown Chicago.
 					</div><!-- END FEATURED EXCERPT -->
-					<a class="cta" href="#">Book Now</a>
+					<a class="cta" href="tour">Book Now</a>
 					<div class="priceDetail">Starting at <span class="featuredPrice">$35.95</span></div>
 				</div><!-- END FEATURED POST -->
 			</div><!-- END WRAP -->
 		</div><!-- END THE DARKENING -->
 	</div><!-- END GLORY -->
-
-
-
-
+	<div class="contentWrap teehee">
+		<div class="wrap">
+			<div class="listHead teehee">
+				<h3>Popular Tours</h3>
+				<a ref="#">View All Tours &rsaquo;</a>
+			</div><!-- END LIST HEADER -->
+			<ul class="tourList yep">
+				<li class="tour">
+					<a href="tour" class="tourHead teehee" style="background-image:url('images/ChicagoRiver.jpg');">
+						<span class="theDarkness"></span>
+						<span class="tourHeadInfo yep">
+							<span class="thTop yep">
+								<span class="tourTime">75 min</span>
+								<span class="rating">
+									<span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span> 
+									<span class="textRating">4.8 of 5 Stars</span>							
+								</span>
+							</span><!-- END TOUR HEADER TOP -->
+							<span class="tourTitle yep">Chicago Architectural Cruise</span>
+							<span class="priceDetail yep">Starting at <span class="featuredPrice">$31.70</span></span>
+						</span><!-- END TOUR HEAD INFO -->
+					</a><!-- END TOUR HEAD -->
+					<div class="tourExcerpt">Enjoy a 75-minute cruise of the historic Chicago River as your guide showcases over 40 landmarks of modern American architecture. <a href="tour">Read More</a>
+					</div><!-- END TOUR EXCERPT -->
+					<a href="tour" class="cta alt">Book This Tour</a>
+				</li><!-- END TOUR -->
+				<li class="tour">
+					<a href="tour" class="tourHead teehee" style="background-image:url('images/ChicagoRiver.jpg');">
+						<span class="theDarkness"></span>
+						<span class="tourHeadInfo yep">
+							<span class="thTop yep">
+								<span class="tourTime">75 min</span>
+								<span class="rating">
+									<span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span> 
+									<span class="textRating">4.8 of 5 Stars</span>							
+								</span>
+							</span><!-- END TOUR HEADER TOP -->
+							<span class="tourTitle yep">Chicago Architectural Cruise</span>
+							<span class="priceDetail yep">Starting at <span class="featuredPrice">$31.70</span></span>
+						</span><!-- END TOUR HEAD INFO -->
+					</a><!-- END TOUR HEAD -->
+					<div class="tourExcerpt">Enjoy a 75-minute cruise of the historic Chicago River as your guide showcases over 40 landmarks of modern American architecture. <a href="tour">Read More</a>
+					</div><!-- END TOUR EXCERPT -->
+					<a href="tour" class="cta alt">Book This Tour</a>
+				</li><!-- END TOUR -->
+				<li class="tour">
+					<a href="tour" class="tourHead teehee" style="background-image:url('images/ChicagoRiver.jpg');">
+						<span class="theDarkness"></span>
+						<span class="tourHeadInfo yep">
+							<span class="thTop yep">
+								<span class="tourTime">75 min</span>
+								<span class="rating">
+									<span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span> 
+									<span class="textRating">4.8 of 5 Stars</span>							
+								</span>
+							</span><!-- END TOUR HEADER TOP -->
+							<span class="tourTitle yep">Chicago Architectural Cruise</span>
+							<span class="priceDetail yep">Starting at <span class="featuredPrice">$31.70</span></span>
+						</span><!-- END TOUR HEAD INFO -->
+					</a><!-- END TOUR HEAD -->
+					<div class="tourExcerpt">Enjoy a 75-minute cruise of the historic Chicago River as your guide showcases over 40 landmarks of modern American architecture. <a href="tour">Read More</a>
+					</div><!-- END TOUR EXCERPT -->
+					<a href="tour" class="cta alt">Book This Tour</a>
+				</li><!-- END TOUR -->		
+			</ul><!-- END TOUR LIST -->
+		</div><!-- END WRAP -->
+	</div><!-- END CONTENT WRAP -->
 <?php require_once('../../footer.php'); ?>
