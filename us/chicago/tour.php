f9f8a3b2cd2165713c49fd74cbc7260cb0c8361e
Cale J Spencer <calejspencer@gmail.com>
2014-03-25 17:59:11 -0400

    Adding Overview and Details information
    Adding and styling info for Overview and Details tabs on tour page

--- us/chicago/tour.php
+++ us/chicago/tour.php
@@ -100,6 +100,15 @@ require_once('../../header.php'); ?>
 						<p>The Explore Tour allows you to hop off to visit a Chicago attraction or museum, shop, dine, and just explore as you have 3 full days to use it. With the same ticket, you also get seasonal access to three Neighborhood Tours (North, West and South) as well as the Nights and Lights Tour (May through September).</p>
 						<p>Your tour can begin at any of the 14 stops in the downtown area. Stops include: Millennium Park, the Art Institute, Field Museum, Navy Pier, John Hancock Observatory, Skydeck Chicago, and the River North district, among many others.</p>
 						<p>Take your time and hop-on/hop-off for three days!</p>
+						<p><strong>Length:</strong> Valid for 3 days</p>
+						<strong>Time:</strong>
+						<ul>
+							<li>Daily departures from 9:00 am to 5:00 pm - Mid-March through early November</li>
+							<li>Daily departures from 9:00 am to 4:00 pm - Early November through mid-March</li>
+							<li><em>Time on availability calendar is for booking purposes only and reflects the first tour departure time each day.</em></li>
+						</ul>
+						<p><strong>Summary:</strong> Pickups available only at designated trolley stops. Neighborhood Tours and Night Tour only available May through September. Child rate applies to children ages 3 to 12. Children under 3 are free. <br><strong>Please order online or call 888-881-3284 to order or for more information.</strong></p>
+						<p>There is a $6.95 USD processing fee per order. This is a flat fee regardless of the number of tickets or tours purchased on an order. There is a service charge per ticket. This charge will be reflected in your summary before you checkout. The total shown includes any and all taxes, fuel surcharges, and service fees. There are no additional charges, unless otherwise specified.</p>
 					</div><!-- END TAB LEFT -->
 				</div>
 				<!-- END OVERVIEW -->
@@ -109,6 +118,58 @@ require_once('../../header.php'); ?>
 					</div><!-- END TAB RIGHT -->
 					<div class="tabLeft">
 						<h2>Tour Detail</h2>
+						<p>With the Explore Chicago ticket, you can go visit a Chicago attraction or museum, shop 'til you drop, or just explore - and you have 3 full days to use it.</p>
+						<p><strong>PLEASE NOTE:</strong> The Neighborhood Tours and City Lights Night Tours only operate May through September.</p>
+						<p>With every ticket purchase, customers enjoy special offers including a souvenir t-shirt, Garrett Popcorn, Chicago-made candy, and much more.</p>
+						<strong>Hop-On-Hop-Off Stops:</strong>
+						<p><em>Departures occur approximately every 20-30 minutes.</em></p>
+						<ul>
+							<li>Millennium Park</li>
+							<li>Michigan Avenue Bridge</li>
+							<li>Garret Popcorn/Theatre District</li>
+							<li>Art Institute</li>
+							<li>Hilton Chicago</li>
+							<li>Field Museum</li>
+							<li>Sheraton Hotel</li>
+							<li>Navy Pier</li>
+							<li>Magnificent Mile</li>
+							<li>Water Tower</li>
+							<li>John Hancock Observatory</li>
+							<li>Hard Rock/River North</li>
+							<li>Skydeck Chicago</li>
+							<li>Chicago Theatre/Macy's on State Street</li>
+						</ul>
+						<em>Specific boarding locations will be noted on your E-Ticket.</em>
+						<p><em>Route subject to change.</em></p>
+						<p><strong>Neighborhood Tours:</strong></p>
+						<em>WEST NEIGHBORHOODS TOUR (Approximately 2 Hours)</em>
+						<ul>
+							<li>Chinatown - Experience the food and shopping.</li>
+							<li>Little Italy - Experience the food and shopping.</li>
+							<li>Greektown - Experience the food and shopping.</li>
+						</ul>
+						<em>SOUTH NEIGHBORHOODS TOUR (Approximately 2 Hours)</em>
+						<ul>
+							<li>Museum of Science & Industry - Explore 14 acres of exhibits that are sure to entertain your brain, and don't miss the 40 foot tornado, the German U-boat submarine, the Apollo 8 space capsule and so much more!</li>
+							<li>President Barack Obama Hometown Highlights will also be included.</li>
+						</ul>
+						<em>NORTH NEIGHBORHOODS TOUR (Approximately 2 Hours)</em><br>
+						<em>*Tour will not run on Cubs Home Game Days; abbreviated schedule on Game Day nights.</em>
+						<ul>
+							<li>Lincoln Park Zoo - Explore the city's largest public park and maybe pilot a paddle-boat in the lagoon.</li>
+							<li>Blue Man Group - If you haven't seen Blue Man Group, you haven't seen Chicago. Everyone loves the hilarious show the Chicago Tribune calls "the perfect entertainment." Show ticket not included in tour price.</li>
+						</ul>
+						<em>CITY LIGHTS NIGHT TOUR (Approximately 2 Hours; last return departure is 10:00 PM)</em>
+						<ul>
+							<li>Magnificent Mile - Shop, shop, shop, and shop. Admire the spectacularly illuminated Wrigley building with the historic Water Tower in the distance.</li>
+							<li>John Hancock Center - Watch the sunset from the Observatory and Open Air Skywalk at 1,000 feet above Michigan Avenue.</li>
+							<li>Navy Pier - Chicago's #1 Attraction! Music, food, IMAX, fireworks and more. Enjoy one-of-a-kind views of the skyline at night from the 150-foot ferris wheel! </li>
+						</ul>
+						<p>Rising from the shores of Lake Michigan, Chicago is a cultural, architectural, and historic showcase. Welcome to your urban adventure! With 7,000 restaurants and 77 neighborhoods, Chicago offers innumerable choices.</p>
+						<p>It's said that all roads lead to Chicago. The trolley staff will help you have the most fun with the greatest ease. With diverse backgrounds, your tour guides and drivers have one thing in common - a passion for Chicago. While you tour everything from underground streets to internationally-acclaimed skyscrapers, they're excited to share their knowledge. Enjoy the ride!</p>
+						<p><em>No boarding or exiting from the trolleys and double deckers except at the designated stops.</em></p>
+						<p><em>Pickups available only at designated trolley stops. Child rate applies to children ages 3 to 12. Children under 3 are free.</em></p>
+						<p><strong>Please order online or call 888-881-3284 to order or for more information.</strong></p>
 					</div><!-- END TAB LEFT -->
 				</div>
 				<!-- END DETAILS -->
